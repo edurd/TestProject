b4d41e16e247631c892bf0a0ecb605d299e519ea
Eduardo Hoyos <edu.hoyos92@gmail.com>
2019-08-26 19:51:20 -0500

    Nuevo proyecto

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Alumno on 8/26/19.
+//  Copyright © 2019 Area51 Training Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
